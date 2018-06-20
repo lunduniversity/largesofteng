e86971c0e0a14609c95576739ec37fa2cedb3278
Rasmus Ros <rasmus.s.ros@gmail.com>
2018-06-20 11:41:27 +0200

    accidentally hard-coded a config parameter

--- base/server/src/main/java/se/lth/base/server/Config.java
+++ base/server/src/main/java/se/lth/base/server/Config.java
@@ -29,7 +29,7 @@ public class Config {
     }
 
     public int getPort() {
-        return 8080;
+        return port;
     }
 
     public String getDatabaseDriver() {
---
+++ base/server/src/main/java/se/lth/base/server/database/MapMapper.java
@@ -0,0 +1,4 @@
+package se.lth.base.server.database;
+
+public class MapMapper {
+}
