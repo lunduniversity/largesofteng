65e26a9f707347be60e500e8ce25dadcae2bde40
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-09-06 08:41:24 +0200

    Refactored foo.js to use less DOM queries, and added the FooViewModel class to encapsulate a table row and its foo

--- base/server/src/main/resources/webassets/foo/foo.js
+++ base/server/src/main/resources/webassets/foo/foo.js
@@ -6,33 +6,48 @@ var base = base || {};
 // Defines the base namespace, if not already declared. Through this pattern it doesn't matter which order
 // the scripts are loaded in.
 base.fooController = function() {
+    'use strict' // add this to avoid some potential bugs
 
-    // List of all foo data, will be useful to have when update functionality is added.
+    // List of all foo data, will be useful to have when update functionality is added in lab 2.
     let model = [];
+
+    const FooViewModel = function(_foo) {
+        // We call the parameter _foo to avoid accidentally using the old version, we might otherwise end up in a
+        // scenario where foo and this.foo are different things.
+        this.foo = _foo;
+        // This assignment is used below where 'this' is not available
+        const viewModel = this;
+
+        this.render = function(template) {
+            this.update(template.content.querySelector('tr'));
+            const clone = document.importNode(template.content, true);
+            this.fooRow = clone.querySelector('tr');
+
+            // TODO: Add stuff from lab 2 end-2-end task here
+
+            template.parentElement.appendChild(clone);
+        };
+        // Update a single table row to display a foo
+        this.update = function(trElement) {
+            const tds = trElement.children;
+            tds[0].textContent = viewModel.foo.payload;
+            const d = viewModel.foo.createdDate;
+            tds[1].textContent = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+
+            // TODO: Add stuff from lab 1 here
+
+        };
+    };
 
     const view = {
         // Creates HTML for each foo in model
         render: function() {
-            model.forEach(d => view.renderPart(d));
-        },
-        // Creates HTML for foo parameter and adds it to the parent of the template
-        renderPart: function(foo) {
-            // Gets a reference to the template.
             // A template element is a special element used only to add dynamic content multiple times.
             // See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
-            const t = view.template();
-            view.update(t.content.querySelector('tr'), foo);
-            const clone = document.importNode(t.content, true);
-            t.parentElement.appendChild(clone);
+            const t = this.template();
+            model.forEach(d => d.render(t));
         },
-        // Update a single table row to display a foo
-        update: function(trElement, foo) {
-            const tds = trElement.children;
-            tds[0].textContent = foo.payload;
-            const d = foo.createdDate;
-            tds[1].textContent = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
-            // TODO: Edit here in lab 1
-        },
+
         template: function() {
             return document.getElementById('foo-template');
         }
@@ -49,7 +64,7 @@ base.fooController = function() {
             // Loads all foos from the server through the REST API, see res.js for definition.
             // It will replace the model with the foos, and then render them through the view.
             base.rest.getFoos().then(function(foos) {
-                model = foos;
+                model = foos.map(f => new FooViewModel(f));
                 view.render();
             });
         },
@@ -62,8 +77,9 @@ base.fooController = function() {
                 .then(function(foo) {
                     // Foo is the response from the server, it will have this form:
                     // {id: 123, userId: 1, payload: 'data', created: 1525343407}
-                    model.push(foo);         // append the foo to the end of the model array
-                    view.renderPart(foo);    // append the foo to the table
+                    const vm = new FooViewModel(foo);
+                    model.push(vm);          // append the foo to the end of the model array
+                    vm.render(view.template());             // append the foo to the table
                     input.value = '';        // clear the input HTML element
                 });
         }
