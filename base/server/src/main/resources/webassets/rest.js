73c598b405d8cf6829d7be8253c3472bff9bf9c4
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-09-06 08:32:19 +0200

    Added documentation to the rest calls

--- base/server/src/main/resources/webassets/rest.js
+++ base/server/src/main/resources/webassets/rest.js
@@ -1,5 +1,11 @@
 var base = base || {};
 base.rest = (function() {
+    'use strict'
+
+    // The available classes are defined here, feel free to move them to other files when this gets unwieldy.
+    // Note the pattern where the class constructor takes an object that is parsed from JSON, and extends
+    // itself using the JSON object with Object.assign.
+    // In this way, we don't have to write: this.id = json.id; this.payload = json.payload etc.
 
     const Foo = function(json) {
         Object.assign(this, json);
@@ -24,9 +30,10 @@ base.rest = (function() {
         };
     };
 
-    base.Foo = Foo;
-    base.User = User;
-    base.Role = Role;
+    // Expose the classes to base module.
+    //base.Foo = Foo;
+    //base.User = User;
+    //base.Role = Role;
 
     // This method extends the functionality of fetch by adding default error handling.
     // Using it is entirely optional.
@@ -57,11 +64,25 @@ base.rest = (function() {
     const jsonHeader = {'Content-Type': 'application/json;charset=utf-8'};
 
     return {
+        /*
+         * Fetches the currently logged in user
+         *
+         * example: const me = base.rest.getUser();
+         */
         getUser: function() {
             return baseFetch('/rest/user')
                 .then(response => response.json())
                 .then(u => new User(u));
         },
+
+        /*
+         * Login with given credentials.
+         * username: name of the user
+         * password: password in plaintext
+         * rememberMe: boolean flag, whether the user
+         *
+         * example: base.rest.login('test', 'password', true);
+         */
         login: function(username, password, rememberMe) {
             var loginObj = {username: username, password: password};
             return baseFetch('/rest/user/login?remember=' + rememberMe, {
@@ -69,19 +90,47 @@ base.rest = (function() {
                     body: JSON.stringify(loginObj),
                     headers: jsonHeader});
         },
+
+        /*
+         * Logout the current user.
+         *
+         * example: base.rest.logout();
+         */
         logout: function() {
             return baseFetch('/rest/user/logout', {method: 'POST'});
         },
+
+        /*
+         * Gets the users available (admin only).
+         * returns: A list of User
+         *
+         * example: const userList = base.rest.getUsers();
+         */
         getUsers: function() {
             return baseFetch('/rest/user/all')
                 .then(response => response.json())
                 .then(users => users.map(u => new User(u)));
         },
+
+        /*
+         * Gets the roles available (admin only).
+         * returns: A list of Role
+         *
+         * example: const availableRoles = base.rest.getRoles();
+         */
         getRoles: function() {
             return baseFetch('/rest/user/roles')
                 .then(response => response.json())
                 .then(roles => roles.map(r => new Role(r)));
         },
+
+        /*
+         * Add a new user (admin only).
+         * credentials: object with username, password, and role
+         * returns: the updated User
+         *
+         * example: let user = base.rest.addUser(2, {'username': 'Test2', 'password': 'password2', 'role': 'USER');
+         */
         addUser: function(credentials) {
             return baseFetch('/rest/user', {
                     method: 'POST',
@@ -90,6 +139,15 @@ base.rest = (function() {
                 .then(response => response.json())
                 .then(u => new User(u));
         },
+
+        /*
+         * Replace a specific user with a given userId (admin only).
+         * id: user to replace
+         * credentials: object with username, password (optional), and role
+         * returns: the updated user
+         *
+         * example: let user = base.rest.putUser(2, {'username': 'Test2', 'role': 'USER');
+         */
         putUser: function(id, credentials) {
             return baseFetch('/rest/user/'+id, {
                     method: 'PUT',
@@ -98,9 +156,23 @@ base.rest = (function() {
                 .then(response => response.json())
                 .then(u => new User(u));
         },
-        deleteUser: function(username) {
-            return baseFetch('/rest/user/'+username, {method: 'DELETE'});
+
+        /*
+         * Delete a specific user with a given userId (admin only).
+         * id: user to delete
+         *
+         * example: base.rest.deleteUser(2);
+         */
+        deleteUser: function(userId) {
+            return baseFetch('/rest/user/'+userId, {method: 'DELETE'});
         },
+
+        /*
+         * Fetches the foos of the user, either the currently logged in one, or of a specific user (admin only).
+         * userId (optional): a specific user OR if userId is not specified.
+         * returns: an array of Foo
+         * example: const someonesFoos = base.rest.getFoos(1);
+         */
         getFoos: function(userId) {
             var postfix = "";
             if (typeof userId !== "undefined") postfix = "/user/" + userId;
@@ -108,6 +180,13 @@ base.rest = (function() {
                 .then(response => response.json())
                 .then(foos => foos.map(f => new Foo(f)));
         },
+
+        /*
+         * Adds foo expects javascript object containing payload
+         * foo: plain javascript object to add
+         * returns: Foo object
+         * example: const myNewFoo = base.rest.addFoo({'payload': 'i wrote this in the input field'});
+         */
         addFoo: function(foo) {
             return baseFetch('/rest/foo', {
                     method: 'POST',
@@ -116,9 +195,22 @@ base.rest = (function() {
                 .then(response => response.json())
                 .then(f => new Foo(f));
         },
+
+        /*
+         * Deletes foo with specific integer id
+         * fooId: id to delete
+         * example: base.rest.deleteFoo(10);
+         */
         deleteFoo: function(fooId) {
             return baseFetch('/rest/foo/'+fooId, {method: 'DELETE'});
         },
+
+        /*
+         * Updates foo with specified integer id with a new integer total
+         * fooId: id to update
+         * total: new total to use
+         * example: base.rest.updateFoo(41, 2);
+         */
         updateFoo: function(fooId, total) {
             return baseFetch('/rest/foo/'+fooId+'/total/'+total, {method: 'POST'})
                 .then(function() {
